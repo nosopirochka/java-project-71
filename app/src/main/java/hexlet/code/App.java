8ed00cc380ce86f25a2484922eff1f19f8683ba7
nosopirochka <dashima.1998@mail.ru>
2024-03-30 14:59:35 +0300

    feat: add App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,41 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import hexlet.code.Differ.Differ;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Override
+    public Integer call() {
+        try {
+            var result = Differ.generate(filepath1, filepath2);
+            System.out.println(result);
+            return 0;
+        } catch (Exception error) {
+            System.out.println("Error: check validity of path and content of file!");
+        }
+        return 0;
+    }
+
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
